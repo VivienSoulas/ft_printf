dfa88061e19c7290e77bddaadb0193e0ab696927
Vivien Soulas <vsoulas@f1r1s8.codam.nl>
2024-11-21 17:31:16 +0100

    main

---
+++ libftprintf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsoulas <vsoulas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 11:53:36 by vsoulas           #+#    #+#             */
+/*   Updated: 2024/11/21 12:20:21 by vsoulas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdarg.h>
+
+int ft_printf(const char *, ...);
+
+#endif
